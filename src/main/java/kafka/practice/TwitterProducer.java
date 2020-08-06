9c0b474702a217217db3e370c220651426dc8c49
Chris <c.lawsonnelson@gmail.com>
2020-08-06 18:32:19 -0400

    initial commit

---
+++ src/main/java/kafka/practice/TwitterProducer.java
@@ -0,0 +1,21 @@
+package kafka.practice;
+
+public class TwitterProducer {
+
+    // constructor
+    public TwitterProducer() {
+
+    }
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public void run() {
+        // TODO create Twitter client
+
+        // TODO create Kafka produce
+
+        // TODO create loop to send tweets to Kafka
+    }
+}
