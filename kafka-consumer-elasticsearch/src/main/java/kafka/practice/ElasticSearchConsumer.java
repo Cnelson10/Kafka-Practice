41be9fbf16a561cb0c74e2726c128ef3b329d51c
Chris <c.lawsonnelson@gmail.com>
2020-08-09 22:45:25 -0400

    test add data into elastic search client index

--- kafka-consumer-elasticsearch/src/main/java/kafka/practice/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/kafka/practice/ElasticSearchConsumer.java
@@ -6,10 +6,18 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class ElasticSearchConsumer {
 
@@ -17,9 +25,9 @@ public class ElasticSearchConsumer {
 
         Dotenv dotenv = Dotenv.load();
 
-        String hostname = dotenv.get("HOSTNAME");
-        String username = dotenv.get("USERNAME");
-        String password = dotenv.get("PASSWORD");
+        String hostname = "kafka-consumer-1075154328.us-east-1.bonsaisearch.net:443";
+        String username = "qr05menh2y";
+        String password = "w5qutadvdm";
 
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
@@ -36,7 +44,26 @@ public class ElasticSearchConsumer {
         return client;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
 
+        // create a client for us to insert data into
+        RestHighLevelClient client = createClient();
+
+        // test data
+        String jsonString = "{ \"test1\": \"testtest\" }";
+
+        // insert json into our index and get back its id
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        logger.info(id);
+
+        // close client
+        client.close();
     }
 }
